1207e45febc5bc720bb004f3592db72d79845859
bnates <brian.nations@codefellows.com>
2020-04-28 11:46:45 -0700

    added oauth demo

--- class-12/demo/oauth-server/index.js
+++ class-12/demo/oauth-server/index.js
@@ -1,41 +1,35 @@
 'use strict';
 
-// 3rd Party Resources
 const express = require('express');
-const users = require('./users.js');
 const basicAuth = require('./basic-auth-middleware.js');
 const oauth = require('./oauth-middleware.js');
+const users = require('./users.js');
 
-// Prepare the express app
 const app = express();
 
-// App Level MW
-app.use(express.static('./public'));
+// will parse the req body on post and put request
 app.use(express.json());
 
-// echo '{"username":"john","password":"foo"}' | http post :3000/signup
+// this is used to serve static content
+app.use(express.static('./public'));
+
 app.post('/signup', (req, res) => {
   users.save(req.body)
     .then(user => {
       let token = users.generateToken(user);
       res.status(200).send(token);
     })
-    .catch(e => { res.status(403).send("Error Creating User"); });
+    .catch( err => res.status(403).send('error creating user'));
 });
 
-// http post :3000/signin -a john:foo
 app.post('/signin', basicAuth, (req, res) => {
+  // req.token only exists because of our basic auth middleware
   res.status(200).send(req.token);
 });
 
+// this is our "redirect_uri"
 app.get('/oauth', oauth, (req, res) => {
   res.status(200).send(req.token);
 });
 
-app.get('/users', basicAuth, (req, res) => {
-  res.status(200).json(users.list());
-});
-
-
-app.listen(3000, () => console.log('server up'));
-
+app.listen(3000, () => { console.log('listening on 3000!')});
---
+++ class-12/demo/oauth-server/users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+let SECRET = 'appsecret'; // this is used as part of jwt for extra auth layers
+
+// here is where you need to focus your lab assignment requirements -> you need mongoose/mongo
+let db = {}; // you need mongo here -> this is your DB for your app
+let users = {}; // you need to create a users model/schema
+
+// these will come in handy
+// mongoose pre hook -> for hashing a pw before saving it (ex: user.pre('save', doStuff))
+
+users.save = async function(record) {
+  console.log('user', record);
+  if (!db[record.username]) {
+    record.password = await bcrypt.hash(record.password, 5);
+    db[record.username] = record;
+
+    return record;
+  }
+
+  return Promise.reject();
+}
+
+// username:pw -> req.headers.authorization
+// decode username:pw
+// check that the decoded pw is the same as the hashed one in the db
+// if so, give me back the user
+users.authenticateBasic = async function(user, pw) {
+  let valid = await bcrypt.compare(pw, db[user].password);
+  return valid ? db[user] : Promise.reject();
+}
+
+// if the user has proper login creds
+// generate a token that will be used in the future for accessing routes in our app
+users.generateToken = function(user) {
+  let token = jwt.sign({ username: user.username}, SECRET);
+  return token;
+}
+
+// give me back a list of users
+users.list = () => db;
+
+module.exports = users;
